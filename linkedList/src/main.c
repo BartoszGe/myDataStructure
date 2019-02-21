1bf1cae13b0bb9657321f95334203f1189ca1757
bgertych <bartosz.gertych@tivo.com>
2019-02-21 15:12:24 +0100

    add push front and print functions

--- linkedList/src/main.c
+++ linkedList/src/main.c
@@ -1,6 +1,69 @@
-#include "stdio.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
 
-int main()
+struct node
 {
-	printf("Hello World");
+	int data;
+    struct node *next;
+};
+
+struct node *head = NULL;
+
+bool const empty(void) 
+{
+    if(head == NULL)
+        return true;
+
+    return false;
+}
+
+size_t const size(void) 
+{
+    if(empty())
+        return 0;
+
+    size_t size = 1;
+
+    for(; head->next!=NULL; ++size);
+
+    return size;
+}
+
+struct node* front_node(void)
+{
+    struct node tempNode = head;
+    (; tempNode->next==NULL; tempNode->)
+} 
+
+void push_front(const int data)
+{
+    struct node *newNode = malloc(sizeof(struct node));
+    
+    if(newNode == NULL) {
+        fprintf(stderr, "malloc in create failed\n");
+        exit(-1);
+    }
+    
+    newNode->data = data;
+    printf("newNode->data: %d\n", newNode->data);
+ 
+    if(empty()) {
+        head = newNode;   
+        return;
+    }
+    
+    head->next = newNode;
+    return;
+}
+
+int main(void)
+{
+    printf("size: %zu\n",size());
+    push_front(1);
+    printf("size: %zu\n",size());
+    push_front(1);
+    printf("size: %zu\n",size());
+    push_front(1);
+    printf("size: %zu\n",size());
 }
