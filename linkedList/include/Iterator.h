7c09504552a9d6a3578a48c64b3739f6b20f2815
bgertych <bartosz.gertych@tivo.com>
2019-02-26 16:38:33 +0100

    change proect from c to c++

---
+++ linkedList/include/Iterator.h
@@ -0,0 +1,14 @@
+#ifndef ITERATOR_H
+#define ITERATOR_H
+
+class Iterator
+{
+    
+    public:
+        Iterator(LinkedList linkedList);
+        Iterator();
+    Iterator begin();
+    Iterator end(); 
+}
+
+#endif
---
+++ linkedList/include/LinkedList.h
@@ -0,0 +1,45 @@
+#ifndef LINKEDLIST_H
+#define LINKEDLIST_H
+
+class LinkedList
+{
+
+
+    private:
+
+        struct node {
+            int data;
+            struct node *next;
+        };
+
+        struct node *head = NULL;
+       
+        Iterator iterator;
+
+    public:
+        
+
+//      Element access
+        int front();
+        int back() {return head->data};
+        
+//      Iterators
+        int begin();
+        int end();
+
+        
+        bool const empty();
+        
+        struct node* front_node();
+        
+        void push_back(const int data);
+        void push_front(const int data);
+        
+    
+
+
+
+
+}
+
+#endif
