d381315b18bab2d2e204f41d46278ed440275497
bartoszge <bartoszgertych94@gmail.com>
2019-05-21 14:45:43 +0200

    add one constructor and reserve method

--- vector/include/vector.h
+++ vector/include/vector.h
@@ -1,9 +1,6 @@
 #ifndef VECTOR_H
 #define VECTOR_H
 
-#include <cstddef>
-#include <stdarg.h>
-
 /* Inspired from: https://en.cppreference.com/w/cpp/container/vector */
 
 template <typename T>
@@ -12,14 +9,18 @@ class Vector
     private:
         size_t mySize;
         size_t myCapacity;
-        T * buffer;
+        T *buffer;
+
     public:
+        //Constructors
         Vector();
-        Vector(unsigned int size);
-        Vector(unsigned int size, T data);
-        // Vector(unsigned int size, const T &referenceVector);
-        // Vector(const T & back, const T & end);
+        Vector(size_t size);
+        Vector(size_t size,const T data);
+        Vector(size_t size, const Vector<T> &referenceVector);
+        Vector(T *begin, T *end);
         Vector(const Vector<T> & vector);
+
+        //Destructor
         ~Vector();
 
         // Iterators
@@ -28,8 +29,16 @@ class Vector
 
         // Capacity
         size_t size() const {return mySize;}
+        size_t resize();
         size_t capacity() const  {return myCapacity;}
-        bool empty() const {return mySize > 0 ?true :false;}
+        bool empty() const {return mySize > 0 ?false :true;}
+        void resize (size_t size, const T& data = 0);
+        void reserve (size_t size);
+
+        // Modifiers
+        void push_back (const T& data);
+
+        T &operator[](size_t idx) {return buffer[idx];}
 };
 
     template <class T>
@@ -41,7 +50,7 @@ class Vector
     }
 
     template <class T>
-    Vector<T>::Vector(unsigned int size)
+    Vector<T>::Vector(size_t size)
     {
         mySize = size;
         myCapacity = size;
@@ -49,30 +58,33 @@ class Vector
     }
 
     template <class T>
-    Vector<T>::Vector(unsigned int size, const T data)
+    Vector<T>::Vector(size_t size, const T data)
     {
         mySize = size;
         myCapacity = size;
         buffer = new T[size];
-        for(unsigned int idx = 0; idx < size; ++idx)
+        for(size_t idx = 0; idx < size; ++idx)
             buffer[idx] = data;
     }
 
-    // template <class T>
-    // Vector<T>::Vector(unsigned int size, const T &referenceVector)
-    // {
-    //     mySize = size;
-    //     myCapacity = size;
-    //     buffer = new T[size];
-    //     for(unsigned int idx = 0; idx < size; ++idx)
-    //         buffer[idx] = referenceVector.buffer[idx];
-    // }
+    template <class T>
+    Vector<T>::Vector(size_t size, const Vector<T> &referenceVector)
+    {
+        mySize = size;
+        myCapacity = size;
+        buffer = new T[size];
+        for(size_t idx = 0; idx < size; ++idx)
+            buffer[idx] = referenceVector.buffer[idx];
+    }
 
-    // template <class T>
-    // Vector<T>::Vector(const T & back, const T & end)
-    // {
+    template <class T>
+    Vector<T>::Vector(T *begin, T *end)
+    {
+        mySize = 0;
 
-    // }
+        for(T* pointer = begin; pointer != end; ++pointer)
+            push_back(*pointer);
+    }
 
     template <class T>
     Vector<T>::Vector(const Vector<T> & vector)
@@ -80,15 +92,42 @@ class Vector
         mySize = vector.size();
         myCapacity = vector.capacity();
         buffer = new T[mySize];
-        for(unsigned int idx = 0; idx < mySize; ++idx)
+        for(size_t idx = 0; idx < mySize; ++idx)
             buffer[idx] = vector.buffer[idx];
     }
 
     template <class T>
     Vector<T>::~Vector()
     {
-        // if(!empty())
+        if(!empty())
             delete []buffer;
+    }
+
+    template <class T>
+    void Vector<T>::resize(size_t size, const T& data) 
+    {
+        
+    }
+
+    template <class T>
+    void Vector<T>::reserve(size_t capacity) 
+    {
+        if(myCapacity >= capacity)
+            return;
+
+        T *newBuffer = new T[capacity];
+        for(int idx = 0; idx < mySize; ++idx)
+            newBuffer[idx] = buffer[idx];
+        
+        delete []buffer;
+        myCapacity = capacity;
+        buffer = newBuffer;
+    }
+
+    template <class T>
+    void Vector<T>::push_back (const T& data)
+    {
+        
     }
 
 #endif
--- vector/test/src/vector_test.cpp
+++ vector/test/src/vector_test.cpp
@@ -2,31 +2,89 @@
 
 #include <cstdint>
 #include "vector.h"
+#include <string>
 
-TEST(constructor, vector)
+TEST(constructorInt, vector)
 {
     Vector<int> firstVector;                                            // empty vector of ints
     Vector<int> secondVector (5);
     Vector<int> thirdVector (4,100);                                    // four ints with value 100
+    Vector<int> fourthVector (4,thirdVector);
     // Vector<int> fourthVector (thirdVector.begin(),thirdVector.end());   // iterating through second
     Vector<int> sixthVector (thirdVector);                             // a copy of third
 
     EXPECT_EQ(100, thirdVector.begin());
     EXPECT_EQ(100, thirdVector.end());
-    // EXPECT_EQ(100, fourthVector.begin());
-    // EXPECT_EQ(100, fourthVector.end());
+    EXPECT_EQ(100, fourthVector.begin());
+    EXPECT_EQ(100, fourthVector.end());
     EXPECT_EQ(100, sixthVector.begin());
     EXPECT_EQ(100, sixthVector.end());
     
-//   // the iterator constructor can also be used to construct from arrays:
-//   int myints[] = {16,2,77,29};
-//   std::vector<int> fifth (myints, myints + sizeof(myints) / sizeof(int) );
-
-//   std::cout << "The contents of fifth are:";
-//   for (std::vector<int>::iterator it = fifth.begin(); it != fifth.end(); ++it)
-//     std::cout << ' ' << *it;
-//   std::cout << '\n';
+  // the iterator constructor can also be used to construct from arrays:
+    int myints[] = {16,2,77,29};
+    int idx = 0;
+    Vector<int> seventhVector (myints, myints + sizeof(myints) / sizeof(int) );
+    // for (std::vector<int>::iterator it = seventhVector.begin(); it != seventhVector.end(); ++it)
+    //     EXPECT_EQ(myints[idx++], *it);
 }
+
+TEST(constructorChar, vector)
+{
+    Vector<char> firstVector;                                            // empty vector of ints
+    Vector<char> secondVector ('c');
+    Vector<char> thirdVector (4,'a');                                    // four ints with value 100
+    Vector<char> fourthVector (4,thirdVector);
+    // Vector<char> fourthVector (thirdVector.begin(),thirdVector.end());   // iterating through second
+    Vector<char> sixthVector (thirdVector);                             // a copy of third
+
+    EXPECT_EQ('a', thirdVector.begin());
+    EXPECT_EQ('a', thirdVector.end());
+    EXPECT_EQ('a', fourthVector.begin());
+    EXPECT_EQ('a', fourthVector.end());
+    EXPECT_EQ('a', sixthVector.begin());
+    EXPECT_EQ('a', sixthVector.end());
+    
+  // the iterator constructor can also be used to construct from arrays:
+    char myints[] = {'a','b','c','d'};
+    int idx = 0;
+    std::vector<char> seventhVector (myints, myints + sizeof(myints) / sizeof(int) );
+    for (std::vector<char>::iterator it = seventhVector.begin(); it != seventhVector.end(); ++it)
+        EXPECT_EQ(myints[idx++], *it);
+}
+
+TEST(reserve, vector)
+{
+    std::vector<int> stdVector;
+    for (int i=1;i<10;i++) stdVector.push_back(i);
+
+    stdVector.reserve(17);
+    std::cout << "stdVector contains:";
+    for (int i=0;i<stdVector.size();i++)
+        std::cout << ' ' << stdVector[i];
+
+    std::cout << "\ncapacity(): " << stdVector.capacity();
+    std::cout << "\nsize():" << stdVector.size();
+
+
+    std::cout << "\nmyVector contains:";
+    Vector<int> myVector (10, 1);   
+    // Vector<int> myVector;
+    // for (int i=1;i<10;i++) myVector.push_back(i);
+
+    for (int i=0;i<myVector.size();i++)
+        std::cout << ' ' << myVector[i];
+    std::cout << "\ncapacity(): " << myVector.capacity();
+    std::cout << "\nsize():" << myVector.size();
+
+    myVector.reserve(15);
+
+    std::cout << "\nNow myVector contains:";
+    for (int i=0;i<myVector.size();i++)
+        std::cout << ' ' << myVector[i];
+    std::cout << "\ncapacity(): " << myVector.capacity();
+    std::cout << "\nsize():" << myVector.size();
+}
+
 
 // TEST(pushTest,vector)
 // {
